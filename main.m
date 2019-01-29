68e1f013c5be8a44836f18adf03688a59dfcd1bb
wangjiwu <874455953@qq.com>
2019-01-29 16:30:37 +0800

    update the explanatory note

--- main.m
+++ main.m
@@ -1,18 +1,14 @@
 clc;
 load shang_datas  % load the data
 
-
+%加载数据  列数表示指标数 ， 行数表示评价的个体数 
+%此数据 7个评价个体 3个评价指标
 X = shang_datas 
 
-rate = 1.4  % set the parameter
-% rate = 1.2   
-% rate = 0.8   
-% rate = 0.6   
+%说明指标是正向指标还是负向指标
+%此数据第一个是负向指标， 其余为正向指标
 Ind=[2 1 1]; %Specify the positive or negative direction of each indicator
 
-for i =  1:7
-    X(i, 2) = X(i, 2) * (1 - rate * X(i, 3)/100) % get new distance 
-end
-
+%S 为分数排名 W为指标权重
 [S,W]=shang(X,Ind) % get the score
 
